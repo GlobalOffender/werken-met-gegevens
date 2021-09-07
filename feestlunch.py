4cbf1a625bf97b55ab7ad32bfc0de23418b05754
GlobalOffender <99067159@mydavinci.nl>
2021-09-07 11:39:39 -0800

    Added calculators

---
+++ feestlunch.py
@@ -0,0 +1,7 @@
+croissant = 17
+croissantprijs = 0.39
+stokbrood = 2
+stokbroodprijs = 2.78
+korting = 1.50
+
+print((croissant * croissantprijs) + (stokbrood * stokbroodprijs) - korting)
---
+++ speelhal.py
@@ -0,0 +1,6 @@
+toegang = 7.45
+personen = 3
+vr = 0.37
+minuten = 9
+
+print((toegang * personen) + (vr * minuten * personen))
