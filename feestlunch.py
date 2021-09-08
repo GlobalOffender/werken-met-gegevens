3f1b3c594e99557dce380132e960ad4207f052e6
GlobalOffender <99067159@mydavinci.nl>
2021-09-08 09:47:27 -0800

    text toegevoegt

--- feestlunch.py
+++ feestlunch.py
@@ -3,5 +3,7 @@ croissantprijs = 0.39
 stokbrood = 2
 stokbroodprijs = 2.78
 korting = 1.50
+bedrag = (croissant * croissantprijs) + (stokbrood * stokbroodprijs) - korting
+text = 'De feestlunch kost je bij de bakker ' + str(bedrag) + ' euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!'
 
-print((croissant * croissantprijs) + (stokbrood * stokbroodprijs) - korting)
+print(text)
--- speelhal.py
+++ speelhal.py
@@ -2,5 +2,7 @@ toegang = 7.45
 personen = 3
 vr = 0.37
 minuten = 9
+bedrag = (toegang * personen) + (vr * minuten * personen)
+text = 'Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar ' + str(bedrag) + ' euro'
 
-print((toegang * personen) + (vr * minuten * personen))
+print(text)
